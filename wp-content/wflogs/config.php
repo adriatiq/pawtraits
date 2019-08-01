fe2b312fd5f1a108756e3f9e7d0dae5afa8be681
adriatiq <adrian.anderson@gmail.com>
2019-08-02 07:32:26 +1000

    Adding html files
    This is the current WP install

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:",4B0zY_!rZ[#R@i-IVd4-izcO6AZMF7|0v[67YrV]$pb*L zm}-6Iyu+@3 b2}cG";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3137;}
